f62f36b5113879d9106ac5d338cc2df3295a8f2e
axeljung01 <axeljung01@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-04-22 09:24:56 +0000

    remove empty test

--- Tests/Unit/Domain/Model/EntryTest.php
+++ Tests/Unit/Domain/Model/EntryTest.php
@@ -1,27 +1,27 @@
 <?php
 
 /***************************************************************
- *  Copyright notice
+ * Copyright notice
  *
- *  (c) 2013 Kevin Schu <kevin.schu@aoemedia.de>, AOE media GmbH
+ * (c) 2013 Kevin Schu <kevin.schu@aoemedia.de>, AOE media GmbH
  *
- *  All rights reserved
+ * All rights reserved
  *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ * This script is part of the TYPO3 project. The TYPO3 project is
+ * free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
  *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *  This copyright notice MUST APPEAR in all copies of the script!
+ * This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
 
 /**
@@ -36,65 +36,39 @@
  *
  * @author Kevin Schu <kevin.schu@aoemedia.de>
  */
-class Tx_FeloginBruteforceProtection_Domain_Model_EntryTest extends Tx_Extbase_Tests_Unit_BaseTestCase
-{
+class Tx_FeloginBruteforceProtection_Domain_Model_EntryTest extends Tx_Extbase_Tests_Unit_BaseTestCase {
 	/**
 	 * @var Tx_FeloginBruteforceProtection_Domain_Model_Entry
 	 */
 	protected $fixture;
-
-	public function setUp()
-	{
-		$this->fixture = new Tx_FeloginBruteforceProtection_Domain_Model_Entry();
+	public function setUp() {
+		$this->fixture = new Tx_FeloginBruteforceProtection_Domain_Model_Entry ();
 	}
-
-	public function tearDown()
-	{
-		unset($this->fixture);
+	public function tearDown() {
+		unset ( $this->fixture );
 	}
-
+	
 	/**
 	 * @test
 	 */
-	public function getIdentifierReturnsInitialValueForString()
-	{
+	public function setIdentifierForStringSetsIdentifier() {
+		$this->fixture->setIdentifier ( 'Conceived at T3CON10' );
+		$this->assertSame ( 'Conceived at T3CON10', $this->fixture->getIdentifier () );
 	}
-
+	
 	/**
 	 * @test
 	 */
-	public function setIdentifierForStringSetsIdentifier()
-	{
-		$this->fixture->setIdentifier('Conceived at T3CON10');
-
-		$this->assertSame(
-			'Conceived at T3CON10',
-			$this->fixture->getIdentifier()
-		);
+	public function getFailuresReturnsInitialValueForInteger() {
+		$this->assertSame ( 0, $this->fixture->getFailures () );
 	}
-
+	
 	/**
 	 * @test
 	 */
-	public function getFailuresReturnsInitialValueForInteger()
-	{
-		$this->assertSame(
-			0,
-			$this->fixture->getFailures()
-		);
-	}
-
-	/**
-	 * @test
-	 */
-	public function setFailuresForIntegerSetsFailures()
-	{
-		$this->fixture->setFailures(12);
-
-		$this->assertSame(
-			12,
-			$this->fixture->getFailures()
-		);
+	public function setFailuresForIntegerSetsFailures() {
+		$this->fixture->setFailures ( 12 );
+		$this->assertSame ( 12, $this->fixture->getFailures () );
 	}
 
 }
