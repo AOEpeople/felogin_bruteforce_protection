302d915694179fcc118d887685bef0f7838e41c1
André Wuttig <wuttig@portrino.de>
2015-07-02 11:20:18 +0200

    * moves Entry TCA configuration into single file * adds CIDRUtility and cidr_match function to check if client IP if within subnet of configured excluded IP Subnet (CIDR Notation!)

---
+++ Classes/Utility/CIDRUtility.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Aoe\FeloginBruteforceProtection\Utility;
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2014 Andre Wuttig <wuttig@portrino.de>, portrino GmbH
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Class CIDRUtility
+ *
+ * @package Aoe\FeloginBruteforceProtection\Utility
+ */
+class CIDRUtility {
+
+    // is ip in subnet
+    // e.g. is 10.5.21.30 in 10.5.16.0/20 == true
+    //      is 192.168.50.2 in 192.168.30.0/23 == false
+    static function cidr_match($ip, $range)
+    {
+        list ($subnet, $bits) = explode('/', $range);
+        $ip = ip2long($ip);
+        $subnet = ip2long($subnet);
+        $mask = -1 << (32 - $bits);
+        $subnet &= $mask; # nb: in case the supplied subnet wasn't correctly aligned
+        return ($ip & $mask) == $subnet;
+    }
+}
