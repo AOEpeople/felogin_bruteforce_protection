4d9a09e2de88cf6840523a764fadf0014873ac89
Tomas Norre Mikkelsen <tomasnorre@gmail.com>
2019-07-25 11:36:20 +0200

    [BUGFIX] Correct use of AuthenticationService (#19)

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-9.5.99',
+            'typo3' => '7.6.0-8.7.99',
         ],
         'conflicts' => [
         ],
