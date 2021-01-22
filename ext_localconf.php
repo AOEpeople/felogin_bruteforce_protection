42d7c4f6d9338c45b9a79c9282b9191e2e6877c3
Oliver Eglseder <oliver.eglseder@in2code.de>
2021-01-22 11:11:08 +0100

    [BUGFX] Use $GLOBALS['TYPO3_CONF_VARS'] instead of $TYPO3_CONF_VARS

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,7 +4,7 @@ defined('TYPO3') or die();
 
 if (TYPO3_MODE == 'FE') {
     // postUserLookUp hookC
-    $TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_userauth.php']['postUserLookUp']['felogin_bruteforce_protection'] = \Aoe\FeloginBruteforceProtection\Hooks\UserAuth\PostUserLookUp::class . '->handlePostUserLookUp';
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_userauth.php']['postUserLookUp']['felogin_bruteforce_protection'] = \Aoe\FeloginBruteforceProtection\Hooks\UserAuth\PostUserLookUp::class . '->handlePostUserLookUp';
 }
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addService(
