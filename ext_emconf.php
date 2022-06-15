ff676d245c0c62643b784d41a0b4113037a02122
Thomas Schenk <Thomas.Schenk@aoe.com>
2022-06-15 11:34:33 +0200

    [RELEASE] v9.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,10 +17,10 @@ $EM_CONF[$_EXTKEY] = [
     'modify_tables' => '',
     'clearCacheOnLoad' => 0,
     'lockType' => '',
-    'version' => '2.1.0',
+    'version' => '9.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-9.5.99',
+            'typo3' => '8.7.0-9.5.99',
         ],
         'conflicts' => [
         ],
