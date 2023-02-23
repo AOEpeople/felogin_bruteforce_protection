152194a53b5395b75ca49f80f4f79fb248d0b3a5
thomas.layh <thomas.layh@aoe.com>
2023-02-23 14:58:38 +0100

    [TASK] remove deprected field loginFailure and add postProcessing hook for login failures

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,6 +5,7 @@ defined('TYPO3') or die();
 if (TYPO3_MODE == 'FE') {
     // postUserLookUp hookC
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_userauth.php']['postUserLookUp']['felogin_bruteforce_protection'] = \Aoe\FeloginBruteforceProtection\Hooks\UserAuth\PostUserLookUp::class . '->handlePostUserLookUp';
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_userauth.php']['postLoginFailureProcessing']['felogin_bruteforce_protection'] = \Aoe\FeloginBruteforceProtection\Hooks\UserAuth\PostUserLookUp::class . '->processFailedLogin';
 }
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addService(
