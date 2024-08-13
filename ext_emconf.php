bff94940216cc4bb7c4660526bacc6a1fd6dacfe
Clemens Queissner <clemens.queissner@cq-design.de>
2024-08-13 14:58:51 +0200

    chore: Updated ext_emconf.php version schema.

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,10 +17,10 @@ $EM_CONF[$_EXTKEY] = [
     'modify_tables' => '',
     'clearCacheOnLoad' => 0,
     'lockType' => '',
-    'version' => '11.0.3',
+    'version' => '12.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-11.5.99',
+            'typo3' => '12.4.0-12.4.99',
         ],
         'conflicts' => [
         ],
